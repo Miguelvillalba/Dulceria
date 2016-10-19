01f3662776c5582f6947cb896517953503126a3e
FamTerrazas <FamTerrazas@FAMTERRAZAS-PC>
2016-10-18 20:50:18 -0500

    ViewProveedor

--- Dulceria/src/models/ModelProducto.java
+++ Dulceria/src/models/ModelProducto.java
@@ -10,5 +10,80 @@ package models;
  * @author Andro
  */
 public class ModelProducto {
+    private int id_producto;
+    private String nombre;
+    private double precio;
+    private String descripcion;
+    private int existencias;
+
+    /**
+     * @return the id_producto
+     */
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    /**
+     * @param id_producto the id_producto to set
+     */
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the existencias
+     */
+    public int getExistencias() {
+        return existencias;
+    }
+
+    /**
+     * @param existencias the existencias to set
+     */
+    public void setExistencias(int existencias) {
+        this.existencias = existencias;
+    }
     
 }
