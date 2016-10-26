4480278ec141f6ded1abd5ee280861cc1ac6342b
Miguel <Miguel@Migue-PC.lan>
2016-10-26 17:49:43 -0500

    model cliente

--- Dulceria/src/models/ModelCliente.java
+++ Dulceria/src/models/ModelCliente.java
@@ -10,5 +10,63 @@ package models;
  * @author Andro
  */
 public class ModelCliente {
-    
+   private String nombre= "";  
+   private String Direccion= "";
+   private String telefono = "";
+   private String ciudad= "";
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the Direccion
+     */
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    /**
+     * @param Direccion the Direccion to set
+     */
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    /**
+     * @return the ciudad
+     */
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    /**
+     * @param ciudad the ciudad to set
+     */
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
 }
