81fbe60ffa055698551f99098c8531a03dc0978f
FamTerrazas <FamTerrazas@192.168.1.70>
2016-10-25 20:57:57 -0500

    ModelProveedor

--- Dulceria/src/models/ModelProveedor.java
+++ Dulceria/src/models/ModelProveedor.java
@@ -1,14 +1,109 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 /**
  *
- * @author Andro
+ * @author Karla
  */
 public class ModelProveedor {
+    private int id_proveedor;
+    private String nombre;
+    private String rfc;
+    private String calle;
+    private int no;
+    private String colonia;
+    private String ciudad;
+    private String estado;
+    private String nombrecontacto;
+    private int telefono;
+    private String email;
+
+
+    public int getId_proveedor() {
+        return id_proveedor;
+    }
+
+    public void setId_proveedor(int id_proveedor) {
+        this.id_proveedor = id_proveedor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNombrecontacto() {
+        return nombrecontacto;
+    }
+
+    public void setNombrecontacto(String nombrecontacto) {
+        this.nombrecontacto = nombrecontacto;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
     
 }
