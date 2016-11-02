aa99a845be607b0adfd5427793328957ffa54fb2
Miguel <Miguel@172.16.7.17>
2016-11-02 11:40:11 -0600

    repare algunos errores
    falta la tabla de proveedor en la base de datos

--- Dulceria/src/controls/ControllerProveedor.java
+++ Dulceria/src/controls/ControllerProveedor.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package controls;
-//import models.ModelProveedor;
+import models.ModelProveedor;
 import views.ViewProveedor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,7 +15,7 @@ import javax.swing.JOptionPane;
  *
  * @author Karla
  */
-/*public class ControllerProveedor implements ActionListener {
+public class ControllerProveedor implements ActionListener {
     
     private ModelProveedor modelproveedor;
     private ViewProveedor viewproveedor;
@@ -48,22 +48,22 @@ public void actionPerformed(ActionEvent e) {
 }
 
 private void jB_primeroActionPerformed(){
-    modelproveedor.movePrimero();
+    modelproveedor.moveFirst();
     showValues();
 }
 
 private void jB_anteriorActionPerformed(){
-    modelproveedor.moveAnterior();
+    modelproveedor.movePrevious();
     showValues();
 }
 
 private void jB_siguienteActionPerformed(){
-    modelproveedor.moveSiguiente();
+    modelproveedor.moveNext();
     showValues();
 }
 
 private void jB_ultimoActionPerformed(){
-    modelproveedor.moveUltimo();
+    modelproveedor.moveLast();
     showValues();
 }
 
@@ -72,12 +72,12 @@ private void showValues(){
     viewproveedor.jtf_nombre.setText(modelproveedor.getNombre());
     viewproveedor.jtf_rfc.setText(modelproveedor.getRfc());
     viewproveedor.jtf_calle.setText(modelproveedor.getCalle());
-    viewproveedor.jtf_no.setText(modelproveedor.getNo());
+    viewproveedor.jtf_no.setText(""+modelproveedor.getNo());
     viewproveedor.jtf_colonia.setText(modelproveedor.getColonia());
     viewproveedor.jtf_ciudad.setText(modelproveedor.getCiudad());
     viewproveedor.jtf_estado.setText(modelproveedor.getEstado());
     viewproveedor.jtf_nombre_contacto.setText(modelproveedor.getNombrecontacto());
-    viewproveedor.jtf_telefono.setText(modelproveedor.getTelefono());
+    viewproveedor.jtf_telefono.setText(""+modelproveedor.getTelefono());
     viewproveedor.jtf_email.setText(modelproveedor.getEmail());
 }
  private void initview() {
@@ -104,21 +104,20 @@ private void showValues(){
     nombre = this.viewproveedor.jtf_nombre.getText();
     rfc = this.viewproveedor.jtf_rfc.getText();
     calle = this.viewproveedor.jtf_calle.getText();
-    no = this.viewproveedor.jtf_no.getText();
+    //no = this.viewproveedor.jtf_no.getText();
     colonia = this.viewproveedor.jtf_colonia.getText();
     ciudad = this.viewproveedor.jtf_ciudad.getText();
     estado = this.viewproveedor.jtf_estado.getText();
     nombre_contacto = this.viewproveedor.jtf_nombre_contacto.getText();
-    telefono = this.viewproveedor.jtf_telefono.getText();
+    //telefono = this.viewproveedor.jtf_telefono.getText();
     email = this.viewproveedor.jtf_email.getText();
 
-    String sql = "insert into proveedor(id,nombre,rfc,calle,no,colonia,ciudad,estado,nombre_contacto,telefono,email) values ('" + id_proveedor + "','" + nombre + "','" + rfc + "','" + calle + "','" + no + "','" + colonia + '",'" + ciudad + "','" + estado + "','" + nombre_contacto + "','" + telefono + "','" + email + "');";
-    System.out.println(sql);
-    st.executeUpdate(sql);
+    //String sql = "insert into proveedor(id,nombre,rfc,calle,no,colonia,ciudad,estado,nombre_contacto,telefono,email) values ('" + id_proveedor + "','" + nombre + "','" + rfc + "','" + calle + "','" + no + "','" + colonia + '",'" + ciudad + "','" + estado + "','" + nombre_contacto + "','" + telefono + "','" + email + "');";
+  //  System.out.println(sql);
+    //st.executeUpdate(sql);
 
     } catch (Exception err) {
     JOptionPane.showMessageDialog(null, "Llena los campos correctamente" + err.getLocalizedMessage());
     }
  }
 }
-*/	  
