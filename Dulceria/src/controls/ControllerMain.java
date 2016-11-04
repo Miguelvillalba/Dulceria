924b2baf6bb834c847e879a68cf5022d63e158a9
Miguel <Miguel@172.16.7.17>
2016-11-03 21:58:21 -0600

    controller

--- Dulceria/src/controls/ControllerMain.java
+++ Dulceria/src/controls/ControllerMain.java
@@ -11,20 +11,27 @@ package controls;
  */
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import views.ViewMain;
-import views.ViewProducto;
-import models.ModelMain;
+import views.*;
+import models.*;
 
 public class ControllerMain implements ActionListener{
     private final ViewMain viewMain;
     private final ViewProducto viewProducto;
+    private final ViewCliente viewCliente;
+    private final ViewProveedor viewProveedor;
     private final ModelMain modelMain;
     
-    public ControllerMain(ViewMain viewMain, ViewProducto viewProducto, ModelMain modelMain){
+    
+    public ControllerMain(ViewMain viewMain, ViewProducto viewProducto,ViewCliente viewCliente,ViewProveedor viewProveedor, ModelMain modelMain){
         this.viewMain = viewMain;
         this.viewProducto = viewProducto;
+        this.viewCliente = viewCliente;
+        this.viewProveedor = viewProveedor;
         this.modelMain = modelMain;
         
+        this.viewMain.jmi_clientes.addActionListener(this);
+        this.viewMain.jmi_productos.addActionListener(this);
+        this.viewMain.jmi_proveedores.addActionListener(this);
         this.viewMain.jmi_productos.addActionListener(this);
         
         initView();
@@ -34,10 +41,27 @@ public class ControllerMain implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==viewMain.jmi_productos)
             jmiContactsActionPerformed();
+        if(e.getSource()==viewMain.jmi_clientes){
+          jmiClientessActionPerformed(); 
+        }
+        else if(e.getSource()==viewMain.jmi_proveedores){
+            jmiProveedoresActionPerformed();
+        }
     }
     
     public void jmiContactsActionPerformed(){
         viewMain.setContentPane(viewProducto);
+        viewMain.revalidate();
+        viewMain.repaint();
+    }
+    public void jmiClientessActionPerformed(){
+        viewMain.setContentPane(viewCliente);
+        viewMain.revalidate();
+        viewMain.repaint();
+    }
+
+    public void jmiProveedoresActionPerformed(){
+        viewMain.setContentPane(viewProveedor);
         viewMain.revalidate();
         viewMain.repaint();
     }
